d01bd6c1f0ea28e12a101148381ffb54fc470089
thenotsotalentedcoder <malikmuzammilarman@gmail.com>
2025-08-30 14:04:38 +0500

    imagesupdates

--- src/components/ImageSlideshow.js
+++ src/components/ImageSlideshow.js
@@ -4,22 +4,26 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 // Import images directly - update these paths to match your actual file names
 import image1 from '../Assets/images/image3.jpg';
-import image2 from '../Assets/images/image5.jpg';
+import image2 from '../Assets/images/image14.jpg';
 import image3 from '../Assets/images/image8.jpg';
 import image4 from '../Assets/images/image11.jpg';
-import image5 from '../Assets/images/image13.jpg';
-import image6 from '../Assets/images/image.jpg';
-import image7 from '../Assets/images/image7.jpg';
+import image5 from '../Assets/images/image17.jpg';
+import image6 from '../Assets/images/image7.jpg';
+import image7 from '../Assets/images/image15.jpg';
 import image8 from '../Assets/images/image10.jpg';
 import image9 from '../Assets/images/image9.jpg';
-import image10 from '../Assets/images/image12.jpg';
+import image10 from '../Assets/images/image16.jpg';
+import image11 from '../Assets/images/image5.jpg';
+import image12 from '../Assets/images/image13.jpg';
+import image13 from '../Assets/images/image6.jpg';
+import image14 from '../Assets/images/image12.jpg';
 
 
 
 const ImageSlideshow = () => {
   // If you have a different image format, modify the extensions accordingly
   const images = [
-    image1, image2, image3, image4, image5, image6, image7, image8, image9, image10
+    image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14
   ];
   
   const [currentIndex, setCurrentIndex] = useState(0);
